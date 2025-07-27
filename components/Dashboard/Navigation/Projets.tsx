934e7b6fe94745c01f1f522abf48f95f22546c38
calebandria <kalebaandriamanaja@gmail.com>
2025-07-27 08:20:46 +0300

    fix: adding real data in project dashboard tantsaha

--- components/Dashboard/Navigation/Projets.tsx
+++ components/Dashboard/Navigation/Projets.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import ProjectsSummary from '~/components/ProjectsSummary';
 import { ProjectsSummaryProps } from '~/components/ProjectsSummary';
 
-import { ScreenContent } from '~/components/ScreenContent';
+
 
 export const simpleProjectsSummaryData: ProjectsSummaryProps = {
   totalProjects: 5,
