064650b4b4005827db2425bf3dd92fd8e18ea295
Sandaadrien <sandaniainaadrien@gmail.com>
2025-07-28 11:25:00 +0300

    modification du terrains

--- app/(tabs)/terrain/index.tsx
+++ app/(tabs)/terrain/index.tsx
@@ -1,29 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>Gestion des terrains</Text>
+      <TouchableOpacity style={styles.button}>
+        <AntDesign name="plus" size={20} color="white" style={styles.icon} />
+        <Text style={styles.text}>Nouveau terrain</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default function TerrainScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Terrain</Text>
+      <Header />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
-    textAlign: 'center',
+  title: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#4d7c0f',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#16a34a',
+    padding: 14,
+    borderRadius: 8,
+    gap: 14,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: 'white',
+  },
+  icon: {
+    fontSize: 15,
+    fontWeight: 600,
   },
 });
